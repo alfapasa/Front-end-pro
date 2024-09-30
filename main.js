044e29af15c98d701f79344411a552cc3835fe9c
liza <lkdigital.ui@gmail.com>
2024-09-30 23:08:46 +0200

    second commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let typeOfData = 1;
+console.log(typeof(typeOfData));
+
+typeOfData = "kdfkjs";
+console.log(typeof(typeOfData));
+
+typeOfData = [1,2,3];
+console.log(typeof(typeOfData));
+
+typeOfData = 1;
+let secondNumber = 2;
+console.log(typeof(typeOfData !== secondNumber));
+
+let thirdVariable;
+console.log(typeof(thirdVariable));
