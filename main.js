dc5d2407fb654a32e86f01f4f2c4839137e70aee
liza <lkdigital.ui@gmail.com>
2024-10-01 00:19:17 +0200

    completed lesson 4 hw1

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
-let fiveDigitsNumber = 10369;
-
-let firstDigit = Math.floor(fiveDigitsNumber / 10000);
-let secondDigit = Math.floor(fiveDigitsNumber % 10000 / 1000);
-let thirdDigit = Math.floor(fiveDigitsNumber % 1000 / 100);
-let fourthDigit = Math.floor(fiveDigitsNumber % 100 / 10);
-let fifthDigit = Math.floor(fiveDigitsNumber % 10);
-
-console.log(`${firstDigit} ${secondDigit} ${thirdDigit} ${fourthDigit} ${fifthDigit}`);
+let userName = prompt('What is your name?');
+alert(`Hello, ${userName}, how are you?`);
