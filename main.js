fb44d46b4b96513d43a91bf896b50d464b7f58f6
liza <lkdigital.ui@gmail.com>
2024-10-01 00:13:15 +0200

    completed lesson 3 hw3

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-let firstString = prompt('Write something');
-let secondtString = prompt('Write something');
-let thirdString = prompt('Write something');
+let fiveDigitsNumber = 10369;
 
-console.log(`you wrote: ${firstString} ${secondtString} ${thirdString}`);
+let firstDigit = Math.floor(fiveDigitsNumber / 10000);
+let secondDigit = Math.floor(fiveDigitsNumber % 10000 / 1000);
+let thirdDigit = Math.floor(fiveDigitsNumber % 1000 / 100);
+let fourthDigit = Math.floor(fiveDigitsNumber % 100 / 10);
+let fifthDigit = Math.floor(fiveDigitsNumber % 10);
+
+console.log(`${firstDigit} ${secondDigit} ${thirdDigit} ${fourthDigit} ${fifthDigit}`);
