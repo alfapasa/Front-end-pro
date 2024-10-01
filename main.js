f43497461d9b413db063f37ab79d07bca9635275
liza <lkdigital.ui@gmail.com>
2024-10-01 23:29:03 +0200

    completed l4hw4

--- main.js
+++ main.js
@@ -1,37 +1,20 @@
-let birthYear = prompt("Type the year your were born");
-let town = prompt("Type the town you live in");
-let favSport = prompt("Type your favourite sport");
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
 
-let message = "";
+switch (true) {
 
-let userAge = 2024 - (+birthYear); 
+    case numOrStr === null:
+    console.log('ви скасували');
+    break;
 
-if (birthYear === null) {
-    message += (` Шкода, що Ви не захотіли ввести свій рік народження`)
-} else {
-    message += (`Currently you ${userAge} years old`);
+    case numOrStr.trim() === '':
+    console.log('Empty String');
+    break;
+
+    case isNaN(+numOrStr):
+    console.log(' number is Ba_NaN');
+    break;
+
+    default:
+    console.log('OK!');
 }
-
-if (town === "Київ") {
-    message += (` Ти живеш у столиці України`);
-} else if (town === "Вашингтон") {
-    message += (` Ти живеш у столиці USA`);
-} else if (town === "Лондон") {
-    message += (` Ти живеш у столиці Великобританії`);
-} else if (town === null) {
-    message += (` Шкода, що Ви не захотіли ввести свій город`)
-} else {
-    message += (` Ти живеш у місті ${town}`);
-}
-
-if (favSport === "бокс") {
-    message += (` Круто! Хочеш стати Кононом МакГрегором?`);
-} else if (favSport === "йога") {
-    message += (` Круто! Хочеш стати сбалансованим?`);
-} else if (favSport === "пілатес") {
-    message += (` Круто! Хочеш займатися за програмою Джозефа Пілатеса?`);
-} else if (favSport === null) {
-    message += (` Шкода, що Ви не захотіли ввести свій улюблений вид спорту`)
-}
-
-alert(message);
