7432311b5cf177ff5cdca9a5e6acb88f930d1827
liza <lkdigital.ui@gmail.com>
2024-10-01 14:45:56 +0200

    completed l4hw2

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-let userName = prompt('What is your name?');
-alert(`Hello, ${userName}, how are you?`);
+let threeDigitsNumber = prompt('Type 3digits number');
+let userNumber = +threeDigitsNumber;
+
+let firstDigit = Math.floor(userNumber / 100);
+let secondDigit = Math.floor(userNumber % 100 / 10);
+let thirdDigit = userNumber % 10;
+
+firstDigit === secondDigit && secondDigit === thirdDigit ? console.log("Yes, it's true, all digits are equal") : console.log("No, it's not true, all digits are not equal");
+firstDigit === secondDigit || secondDigit === thirdDigit || firstDigit === thirdDigit ? console.log("Yes, there are the same digits") : console.log("None of these digits are equal");
