05b70357ab34052239d9d55bf3ebb1853d64b76f
liza <lkdigital.ui@gmail.com>
2024-10-03 16:39:11 +0200

    completed l5hw4

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-//Дано ціле число N (ввести через prompt). Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N
+//Дано ціле число (ввести через 'prompt').
+//З'ясувати, чи просто воно (простим називається число, більше 1, що не має інших дільників, крім 1 і себе).
 
-let number = +prompt('Type the number');
+let number = +prompt("Type the number");
+let isPrime = true;
 
-for (let i = 1; i <= 100 && i * i < number; i++) {
-    console.log(i);
+for (let i = 2; i < number && number > 1; i++) {
+    if (number % i === 0) {
+        isPrime = false;
+        break;
+    };
 }
+
+if (isPrime) {
+    console.log(`${number} is a prime number`);
+} else {
+    console.log(`${number} isn't a prime number`);
+};
+
+
